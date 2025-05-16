b6a820376204d6653cb8d53cf2e5065a8fbbdef0
Payal Saini <sainipihu1235@gmail.com>
2025-05-16 20:29:54 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     <header className="w-full bg-gradient-to-r from-purple-700 to-blue-600 p-6 shadow-xl flex items-center justify-center gap-4">
       <FaFilm className="text-yellow-300 text-8xl drop-shadow-lg" />
       <h1 className="text-4xl font-extrabold text-white drop-shadow-lg tracking-wider !text-white">
-        Movie Plot Generator
+        //changes here
       </h1>
     </header>
   );
